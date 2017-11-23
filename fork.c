fa46b24d7222b298af4436ba59bc91aad010c03f
shawrylk <33708806+shawrylk@users.noreply.github.com>
2017-11-23 21:43:23 +0700

    Update fork.c
    cleaning

--- fork.c
+++ fork.c
@@ -6,31 +6,16 @@
 int main()
 {
 	pid_t child_pid;
-	int flag = 1;
 	printf("the main program proces ID is %d\n ", (int)getpid());
 	child_pid = fork();
 	if (child_pid != 0)
 	{
 		printf("this is the parent process, with ID %d\n ", (int)getpid());
 		printf("the child process ID is %d\n ",(int)child_pid);
-		if (flag == 1)
-		{
-						flag = 0;
-
-			printf("parent flag\n");
-		}
 	}
 	else
 	{
 		printf ("this is the child process, with ID %d\n",(int)getpid());
-	//getchar();
-	//getchar();
-	if (flag == 1)
-		{
-						flag = 0;
-
-			printf("child flag\n");
-		}
 	}
 	return 0;
 }
